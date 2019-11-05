bd07c6757fc047689578cfc2cacfcc3baed1939c
J. Christian Romero <jcromerohdz@gmail.com>
2019-11-05 11:39:57 -0800

    Adding the users controllers and views

---
+++ app/scripts/rest_requestor.rb
@@ -0,0 +1,26 @@
+# app/script/rest_requestor.rb
+require 'rest-client'
+
+def call_url(value)
+  if value == 'new'
+    url = "http://localhost:3000/users/new"
+    print_url(url)
+  elsif value == 'edit'
+    url = "http://localhost:3000/users/1/edit"
+    print_url(url)
+  elsif value == 'show'
+    url = "http://localhost:3000/users/1"
+    print_url(url)
+  else
+    url = "http://localhost:3000/users/"
+    print_url(url)
+  end
+end
+
+def print_url(url)
+  puts RestClient.get(url)
+end
+
+call_url('new')
+call_url('edit')
+call_url('show')
